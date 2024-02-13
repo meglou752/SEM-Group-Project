58d6e998159457e5b04fe67a58e72f5421889165
meglou782 <meganmcleod@Megans-MacBook-Pro-4.local>
2024-02-13 22:13:13 +0000

    Changes to JAR; making displayCountries reusable; removing unnecessary array and lists, returning resultset.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,22 +1,20 @@
 package com.napier.sem;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
-public class App
-{
-    public static void main(String[] args)
-    {
+public class App {
+    public static void main(String[] args) {
         // Create new Application
         App a = new App();
 
         // Connect to database
         a.connect();
 
-        Country[] countries = a.getCountryDescending();
+        // Get ResultSet of countries
+        ResultSet resultSet = a.getCountryDescending();
 
-        a.displayCountries(countries);
+        // Display countries
+        a.displayCountries(resultSet);
 
         // Disconnect from database
         a.disconnect();
@@ -30,39 +28,29 @@ public class App
     /**
      * Connect to the MySQL database.
      */
-    public void connect()
-    {
-        try
-        {
+    public void connect() {
+        try {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
         int retries = 10;
-        for (int i = 0; i < retries; ++i)
-        {
+        for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
-            try
-            {
+            try {
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
-            }
-            catch (SQLException sqle)
-            {
+            } catch (SQLException sqle) {
                 System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
+            } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
@@ -71,24 +59,19 @@ public class App
     /**
      * Disconnect from the MySQL database.
      */
-    public void disconnect()
-    {
-        if (con != null)
-        {
-            try
-            {
+    public void disconnect() {
+        if (con != null) {
+            try {
                 // Close connection
                 con.close();
                 System.out.println(("Connection closed"));
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 System.out.println("Error closing connection to database");
             }
         }
     }
 
-    public Country[] getCountryDescending() {
+    public ResultSet getCountryDescending() {
         try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
@@ -98,23 +81,8 @@ public class App
                             "FROM country\n" +
                             "ORDER BY country.population DESC;";
 
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
-            // Create a list to hold employees
-            List<Country> countryList = new ArrayList<>();
-            // Iterate through the result set and add employees to the list
-            while (rset.next()) {
-                Country country = new Country();
-                country.code = rset.getString("code");
-                country.name = rset.getString("name");
-                country.continent = rset.getString("continent");
-                country.region = rset.getString("region");
-                country.population = rset.getInt("population");
-                country.capital = rset.getInt("capital");
-                countryList.add(country);
-            }
-            // Convert list to array
-            return countryList.toArray(new Country[0]);
+            // Execute SQL statement and return ResultSet
+            return stmt.executeQuery(strSelect);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("Failed to get country details");
@@ -122,12 +90,30 @@ public class App
         }
     }
 
-    public void displayCountries(Country[] countries) {
-        if (countries != null && countries.length > 0) {
-            for (Country country : countries) {
-                System.out.println(
-                        "Country code: " + country.code + " Country name: " + country.name + " Continent: " + country.continent + " Region: " +
-                                country.region + " Population: " + country.population + " Capital: " + country.capital + "\n");
+    //display function which takes resultset as a parameter, allowing more flexibility later
+    public void displayCountries(ResultSet resultSet) {
+        try {
+            // Iterate through the ResultSet and print country details
+            while (resultSet.next()) {
+                String code = resultSet.getString("code");
+                String name = resultSet.getString("name");
+                String continent = resultSet.getString("continent");
+                String region = resultSet.getString("region");
+                int population = resultSet.getInt("population");
+                int capital = resultSet.getInt("capital");
+
+                System.out.println("Country code: " + code + " Country name: " + name + " Continent: " + continent +
+                        " Region: " + region + " Population: " + population + " Capital: " + capital + "\n");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to display country details");
+        } finally {
+            // Close the ResultSet to free resources
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                System.out.println("Error closing ResultSet");
             }
         }
     }
