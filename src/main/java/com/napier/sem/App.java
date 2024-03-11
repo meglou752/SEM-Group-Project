48fd612185f2d3c661da3aa88a86510fdac58d82
Charlene Mellis <40599396@live.napier.ac.uk>
2024-03-11 20:11:21 +0000

    Created use case 5

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -27,13 +27,14 @@ public class App {
         // ResultSet resultSet = c.topNPopulatedCountries(a.con, 5);
         // ResultSet resultSet = c.getCountryDescending(a.con);
         // ResultSet resultSet = c.getContinentDescending(a.con, "Asia");
+        ResultSet resultSet = c.topNPopulatedCountriesContinent(a.con, "Asia", 4);
 
         // ResultSet resultSet = d.topNPopulatedCitiesDistrict(a.con, "Noord-Brabant", 3);
         // ResultSet resultSet = d.topNPopulatedCities(a.con, 5);
         // ResultSet resultSet = d.getTopRegionCityDescending(a.con, "Western Europe", 6);
         // ResultSet resultSet = d.getCitiesInContinentDesc(a.con, "Asia");
         // ResultSet resultSet = d.getCountryCityDescending(a.con, "France");
-        ResultSet resultSet = d.topNPopulatedCitiesContinent(a.con, "Europe", 5);
+        //ResultSet resultSet = d.topNPopulatedCitiesContinent(a.con, "Europe", 5);
 
         // ResultSet resultSet = e.topNPopulatedCapitals(a.con, 5);
         // ResultSet resultSet = e.getNRegionCapitalsDescending(a.con, "Western Europe", 6);
@@ -47,8 +48,8 @@ public class App {
        // ResultSet resultSet = f.getCountryPopulation(a.con, "Germany");
 
 
-        //c.displayCountries(resultSet);
-        d.displayCities(resultSet);
+        c.displayCountries(resultSet);
+        //d.displayCities(resultSet);
         //e.displayCapitals(resultSet);
         //f.displayPopulations(resultSet);
 
