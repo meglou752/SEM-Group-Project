bcd27d221f6c138fee63a2f7d652fb930dbf60a1
debrx <amadeborah11@gmail.com>
2024-03-12 10:45:43 +0000

    Use case 3 update, app.java properly called

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -43,7 +43,7 @@ public class App {
         // ResultSet resultSet = e.getNRegionCapitalsDescending(a.con, "Western Europe", 6);
         //ResultSet resultSet = e.getContinentCapitalsDescending(a.con, "Asia");
         //ResultSet resultSet = e.topNPopulatedCapitalsByContinent(a.con, "Europe", 5);
-          ResultSet resultSet = e.getCapitalsPopulationDesc(a.con);
+        //ResultSet resultSet = e.getCapitalsPopulationDesc(a.con);
 
         //ResultSet resultSet = f.getWorldPopulation(a.con);
         // ResultSet resultSet = f.getDistrictPopulation(a.con,  "Constantine");
@@ -51,9 +51,9 @@ public class App {
        // ResultSet resultSet = f.getCountryPopulation(a.con, "Germany");
 
 
-        //c.displayCountries(resultSet);
+        c.displayCountries(resultSet);
         //d.displayCities(resultSet);
-        e.displayCapitals(resultSet);
+        //e.displayCapitals(resultSet);
         //f.displayPopulations(resultSet);
 
 
