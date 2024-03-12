52cc454e43ce0328f4572818502779b093a8f681
Jack Millar <jackomillar@outlook.com>
2024-03-12 17:38:29 +0000

    Fixing the merge problems

--- src/test/Java/com/napier/sem/CountryTest.java
+++ src/test/Java/com/napier/sem/CountryTest.java
@@ -24,10 +24,37 @@ class CountryTest {
 
     @Test
         // Test getTopRegionDescending function
-    void testGetTopRegionDescending() {
+    void getTopRegionDescending_NotNullTest() {
         try {
             ResultSet resultSet = c.getTopRegionDescending(a.con, "Southern and Central Asia", 5);
             assertNotNull(resultSet);
+        } catch (Exception e) {
+            fail("Exception thrown");
+        }
+    }
+   @Test
+    void getTopRegionDescending_NullTest() {
+        try {
+            ResultSet resultSet = c.getTopRegionDescending(a.con, "Southern and Central Asia", 5);
+            assertNull(resultSet);
+        } catch (Exception e) {
+            fail("Exception thrown");
+        }
+    }
+
+    void getTopRegionDescending_NegativeNTest() {
+        try {
+            ResultSet resultSet = c.getTopRegionDescending(a.con, "Southern and Central Asia", -1);
+            assertNull(resultSet);
+        } catch (Exception e) {
+            fail("Exception thrown");
+        }
+    }
+    @Test
+    void getTopRegionDescending_NullConTest() {
+        try {
+            ResultSet resultSet = c.getTopRegionDescending(null, "Southern and Central Asia", 5);
+            assertNull(resultSet);
         } catch (Exception e) {
             fail("Exception thrown");
         }
