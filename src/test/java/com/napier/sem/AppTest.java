791d9b2a1505eb47b569ee8b4f6abd458ce7f6ae
meglou782 <meganmcleod@Megans-MacBook-Pro-4.local>
2024-03-13 21:43:47 +0000

    Adding AppTest unit tests

--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -1,14 +1,31 @@
 package com.napier.sem;
 
 import org.junit.jupiter.api.*;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class AppTest {
+    static App a;
 
-class AppTest
-{
+    @BeforeAll
+    static void init() {
+        a = new App();
+    }
+
+    //    @Test
+    // Testing connection to the database
+    void testDatabaseConnection() {
+        assertNotNull(a.con);
+    }
+
     @Test
-    void unitTest()
-    {
-        assertEquals(5, 5);
+        // Testing disconnection from the database
+    void testDatabaseDisconnection() {
+        assertNull(a.con);
     }
 }
