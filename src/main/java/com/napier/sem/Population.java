48e39d1df498901d91d3ef2aa3f357540f8ce0f6
meglou782 <meganmcleod@Megans-MacBook-Pro-4.local>
2024-04-18 08:06:34 +0100

    Adding get population by language

--- src/main/java/com/napier/sem/Population.java
+++ src/main/java/com/napier/sem/Population.java
@@ -374,7 +374,7 @@ public class Population {
             // Create string for SQL statement
             String strSelect =
                     "SELECT \n" +
-                            "   countrylanguage.Language AS Language, \n" +
+                            "   countrylanguage.Language AS name, \n" +
                             "   SUM(country.population) AS population, \n" +
                             "    SUM(city.population) AS UrbanPop, \n" +
                             "    ROUND(SUM(city.population) / SUM(country.population) * 100, 1) AS UrbanPopPercentage, \n" +
